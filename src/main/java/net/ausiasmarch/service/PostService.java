849f3b168737c7b498a12ed5216e6bdc6e2a7c73
Sergi Bosch <sergiboschbosch@gmail.com>
2019-11-03 23:05:44 +0100

    Se ha añadido un control de usuario a las funciones insert, update y remove

--- src/main/java/net/ausiasmarch/service/PostService.java
+++ src/main/java/net/ausiasmarch/service/PostService.java
@@ -13,6 +13,8 @@ import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import net.ausiasmarch.bean.BeanInterface;
 
 import net.ausiasmarch.bean.PostBean;
@@ -25,204 +27,227 @@ import net.ausiasmarch.setting.ConnectionSettings;
 
 public class PostService implements ServiceInterface {
 
-    HttpServletRequest oRequest = null;
-    String[] frasesInicio = {"El fin de la estructura ", "La agrupacion logica ", "El objetivo conjunto ", "Una dinámica apropiada "};
-    String[] frasesMitad = {"dirige los objetivos ", "garantiza el deseo ", "mejora la capacidad ", "recupera el concepto "};
-    String[] frasesFinal = {"de la reestructuracion requerida. ", "en la aplicación. ", "por sus innumerables beneficios. ", "contra la inficiencia. "};
+	HttpServletRequest oRequest = null;
+	String[] frasesInicio = { "El fin de la estructura ", "La agrupacion logica ", "El objetivo conjunto ",
+			"Una dinámica apropiada " };
+	String[] frasesMitad = { "dirige los objetivos ", "garantiza el deseo ", "mejora la capacidad ",
+			"recupera el concepto " };
+	String[] frasesFinal = { "de la reestructuracion requerida. ", "en la aplicación. ",
+			"por sus innumerables beneficios. ", "contra la inficiencia. " };
 
-    public PostService(HttpServletRequest oRequest) {
-        this.oRequest = oRequest;
-    }
+	public PostService(HttpServletRequest oRequest) {
+		this.oRequest = oRequest;
+	}
 
-    @Override
-    public String get() throws SQLException {
-        ConnectionInterface oConnectionImplementation = ConnectionFactory.getConnection(ConnectionSettings.connectionPool);
-        Connection oConnection = oConnectionImplementation.newConnection();
-        int id = Integer.parseInt(oRequest.getParameter("id"));
-        PostDao oPostDao = new PostDao(oConnection);
-        PostBean oPostBean = oPostDao.get(id);
-        Gson oGson = GsonFactory.getGson();
-        String strJson = oGson.toJson(oPostBean);
-        if (oConnection != null) {
-            oConnection.close();
-        }
-        if (oConnectionImplementation != null) {
-            oConnectionImplementation.disposeConnection();
-        }
-        return "{\"status\":200,\"message\":" + strJson + "}";
-    }
+	@Override
+	public String get() throws SQLException {
+		ConnectionInterface oConnectionImplementation = ConnectionFactory
+				.getConnection(ConnectionSettings.connectionPool);
+		Connection oConnection = oConnectionImplementation.newConnection();
+		int id = Integer.parseInt(oRequest.getParameter("id"));
+		PostDao oPostDao = new PostDao(oConnection);
+		PostBean oPostBean = oPostDao.get(id);
+		Gson oGson = GsonFactory.getGson();
+		String strJson = oGson.toJson(oPostBean);
+		if (oConnection != null) {
+			oConnection.close();
+		}
+		if (oConnectionImplementation != null) {
+			oConnectionImplementation.disposeConnection();
+		}
+		return "{\"status\":200,\"message\":" + strJson + "}";
+	}
 
-    @Override
-    public String getPage() throws SQLException {
-        ConnectionInterface oConnectionImplementation = ConnectionFactory.getConnection(ConnectionSettings.connectionPool);
-        Connection oConnection = oConnectionImplementation.newConnection();
-        int iRpp = Integer.parseInt(oRequest.getParameter("rpp"));
-        int iPage = Integer.parseInt(oRequest.getParameter("page"));
-        List<String> orden = null;
-        if (oRequest.getParameter("order")!=null) {
-        	orden = Arrays.asList(oRequest.getParameter("order").split("\\s*,\\s*"));
-        }
-        PostDao oPostDao = new PostDao(oConnection);
-        ArrayList alPostBean = oPostDao.getPage(iPage, iRpp, orden);
-        Gson oGson = GsonFactory.getGson();
-        String strJson = oGson.toJson(alPostBean);
-        if (oConnection != null) {
-            oConnection.close();
-        }
-        if (oConnectionImplementation != null) {
-            oConnectionImplementation.disposeConnection();
-        }
-        return "{\"status\":200,\"message\":" + strJson + "}";
-    }
+	@Override
+	public String getPage() throws SQLException {
+		ConnectionInterface oConnectionImplementation = ConnectionFactory
+				.getConnection(ConnectionSettings.connectionPool);
+		Connection oConnection = oConnectionImplementation.newConnection();
+		int iRpp = Integer.parseInt(oRequest.getParameter("rpp"));
+		int iPage = Integer.parseInt(oRequest.getParameter("page"));
+		List<String> orden = null;
+		if (oRequest.getParameter("order") != null) {
+			orden = Arrays.asList(oRequest.getParameter("order").split("\\s*,\\s*"));
+		}
+		PostDao oPostDao = new PostDao(oConnection);
+		ArrayList alPostBean = oPostDao.getPage(iPage, iRpp, orden);
+		Gson oGson = GsonFactory.getGson();
+		String strJson = oGson.toJson(alPostBean);
+		if (oConnection != null) {
+			oConnection.close();
+		}
+		if (oConnectionImplementation != null) {
+			oConnectionImplementation.disposeConnection();
+		}
+		return "{\"status\":200,\"message\":" + strJson + "}";
+	}
 
-    @Override
-    public String getCount() throws SQLException {
-        ConnectionInterface oConnectionImplementation = ConnectionFactory.getConnection(ConnectionSettings.connectionPool);
-        Connection oConnection = oConnectionImplementation.newConnection();
-        PostDao oPostDao = new PostDao(oConnection);
-        int iCount = oPostDao.getCount();
-        if (oConnection != null) {
-            oConnection.close();
-        }
-        if (oConnectionImplementation != null) {
-            oConnectionImplementation.disposeConnection();
-        }
-        if (iCount < 0) {
-            return "{\"status\":500,\"message\":" + iCount + "}";
-        } else {
-            return "{\"status\":200,\"message\":" + iCount + "}";
-        }
-    }
+	@Override
+	public String getCount() throws SQLException {
+		ConnectionInterface oConnectionImplementation = ConnectionFactory
+				.getConnection(ConnectionSettings.connectionPool);
+		Connection oConnection = oConnectionImplementation.newConnection();
+		PostDao oPostDao = new PostDao(oConnection);
+		int iCount = oPostDao.getCount();
+		if (oConnection != null) {
+			oConnection.close();
+		}
+		if (oConnectionImplementation != null) {
+			oConnectionImplementation.disposeConnection();
+		}
+		if (iCount < 0) {
+			return "{\"status\":500,\"message\":" + iCount + "}";
+		} else {
+			return "{\"status\":200,\"message\":" + iCount + "}";
+		}
+	}
 
-    @Override
-    public String update() throws SQLException {
-        ConnectionInterface oConnectionImplementation = ConnectionFactory.getConnection(ConnectionSettings.connectionPool);
-        Connection oConnection = oConnectionImplementation.newConnection();
-        Gson oGson = GsonFactory.getGson();
-        ResponseBean oResponseBean;
-        PostBean oPostBean = new PostBean();
-        String data = oRequest.getParameter("data");
-        oPostBean = oGson.fromJson(data, PostBean.class);
-        PostDao oPostDao = new PostDao(oConnection);
-        if (oPostDao.update(oPostBean) == 0) {
-            oResponseBean = new ResponseBean(500, "KO");
-        } else {
-            oResponseBean = new ResponseBean(200, "OK");
-        }
-        if (oConnection != null) {
-            oConnection.close();
-        }
-        if (oConnectionImplementation != null) {
-            oConnectionImplementation.disposeConnection();
-        }
-        return oGson.toJson(oResponseBean);
-    }
+	@Override
+	public String update() throws SQLException {
+		ResponseBean oResponseBean;
+		HttpSession oSession = oRequest.getSession();
+		if (oSession.getAttribute("usuario") == null) {
+			oResponseBean = new ResponseBean(500, "Inicie sesión para acceder a esta función");
+		}
+		ConnectionInterface oConnectionImplementation = ConnectionFactory
+				.getConnection(ConnectionSettings.connectionPool);
+		Connection oConnection = oConnectionImplementation.newConnection();
+		Gson oGson = GsonFactory.getGson();
+		PostBean oPostBean = new PostBean();
+		String data = oRequest.getParameter("data");
+		oPostBean = oGson.fromJson(data, PostBean.class);
+		PostDao oPostDao = new PostDao(oConnection);
+		if (oPostDao.update(oPostBean) == 0) {
+			oResponseBean = new ResponseBean(500, "KO");
+		} else {
+			oResponseBean = new ResponseBean(200, "OK");
+		}
+		if (oConnection != null) {
+			oConnection.close();
+		}
+		if (oConnectionImplementation != null) {
+			oConnectionImplementation.disposeConnection();
+		}
+		return oGson.toJson(oResponseBean);
+	}
 
-    @Override
-    public String getAll() throws SQLException {
-        ConnectionInterface oConnectionImplementation = ConnectionFactory.getConnection(ConnectionSettings.connectionPool);
-        Connection oConnection = oConnectionImplementation.newConnection();
-        PostDao oPostDao = new PostDao(oConnection);
-        Gson oGson = GsonFactory.getGson();
-        String message = "";
-        List<BeanInterface> listaPostBean = oPostDao.getAll();
-        if (listaPostBean == null) {
-            message = "\"La lista está vacia\"";
-        } else {
-            //oGson = gh.getGson();
-            message = oGson.toJson(listaPostBean);
-        }
-        if (oConnection != null) {
-            oConnection.close();
-        }
-        if (oConnectionImplementation != null) {
-            oConnectionImplementation.disposeConnection();
-        }
-        return "{\"status\":200,\"message\":" + message + "}";
-    }
+	@Override
+	public String getAll() throws SQLException {
+		ConnectionInterface oConnectionImplementation = ConnectionFactory
+				.getConnection(ConnectionSettings.connectionPool);
+		Connection oConnection = oConnectionImplementation.newConnection();
+		PostDao oPostDao = new PostDao(oConnection);
+		Gson oGson = GsonFactory.getGson();
+		String message = "";
+		List<BeanInterface> listaPostBean = oPostDao.getAll();
+		if (listaPostBean == null) {
+			message = "\"La lista está vacia\"";
+		} else {
+			// oGson = gh.getGson();
+			message = oGson.toJson(listaPostBean);
+		}
+		if (oConnection != null) {
+			oConnection.close();
+		}
+		if (oConnectionImplementation != null) {
+			oConnectionImplementation.disposeConnection();
+		}
+		return "{\"status\":200,\"message\":" + message + "}";
+	}
 
-    @Override
-    public String insert() throws SQLException {
-        ConnectionInterface oConnectionImplementation = ConnectionFactory.getConnection(ConnectionSettings.connectionPool);
-        Connection oConnection = oConnectionImplementation.newConnection();
-        final GsonBuilder builder = new GsonBuilder();
-        builder.excludeFieldsWithoutExposeAnnotation();
-        Gson oGson = GsonFactory.getGson();
-        PostBean oPostBean = oGson.fromJson(oRequest.getParameter("data"), PostBean.class);
-        ResponseBean oResponseBean;
-        PostDao oPostDao = new PostDao(oConnection);
-        if (oPostDao.insert(oPostBean) == 0) {
-            oResponseBean = new ResponseBean(500, "KO");
-        } else {
-            oResponseBean = new ResponseBean(200, "OK");
-        };
-        if (oConnection != null) {
-            oConnection.close();
-        }
-        if (oConnectionImplementation != null) {
-            oConnectionImplementation.disposeConnection();
-        }
-        return oGson.toJson(oResponseBean);
+	@Override
+	public String insert() throws SQLException {
+		ResponseBean oResponseBean;
+		HttpSession oSession = oRequest.getSession();
+		if (oSession.getAttribute("usuario") == null) {
+			oResponseBean = new ResponseBean(500, "Inicie sesión para acceder a esta función");
+		}
+		ConnectionInterface oConnectionImplementation = ConnectionFactory
+				.getConnection(ConnectionSettings.connectionPool);
+		Connection oConnection = oConnectionImplementation.newConnection();
+		final GsonBuilder builder = new GsonBuilder();
+		builder.excludeFieldsWithoutExposeAnnotation();
+		Gson oGson = GsonFactory.getGson();
+		PostBean oPostBean = oGson.fromJson(oRequest.getParameter("data"), PostBean.class);
+		PostDao oPostDao = new PostDao(oConnection);
+		if (oPostDao.insert(oPostBean) == 0) {
+			oResponseBean = new ResponseBean(500, "KO");
+		} else {
+			oResponseBean = new ResponseBean(200, "OK");
+		}
+		;
+		if (oConnection != null) {
+			oConnection.close();
+		}
+		if (oConnectionImplementation != null) {
+			oConnectionImplementation.disposeConnection();
+		}
+		return oGson.toJson(oResponseBean);
 
-    }
+	}
 
-    @Override
-    public String remove() throws SQLException {
-        ConnectionInterface oConnectionImplementation = ConnectionFactory.getConnection(ConnectionSettings.connectionPool);
-        Connection oConnection = oConnectionImplementation.newConnection();
-        PostDao oPostDao = new PostDao(oConnection);
-        Gson oGson = GsonFactory.getGson();
-        int id = Integer.parseInt(oRequest.getParameter("id"));
-        ResponseBean oResponseBean;
-        if (oPostDao.remove(id) == 0) {
-            oResponseBean = new ResponseBean(500, "KO");
-        } else {
-            oResponseBean = new ResponseBean(200, "OK");
-        }
-        if (oConnection != null) {
-            oConnection.close();
-        }
-        if (oConnectionImplementation != null) {
-            oConnectionImplementation.disposeConnection();
-        }
-        return oGson.toJson(oResponseBean);
-    }
+	@Override
+	public String remove() throws SQLException {
+		ResponseBean oResponseBean;
+		HttpSession oSession = oRequest.getSession();
+		if (oSession.getAttribute("usuario") == null) {
+			oResponseBean = new ResponseBean(500, "Inicie sesión para acceder a esta función");
+		}
+		ConnectionInterface oConnectionImplementation = ConnectionFactory
+				.getConnection(ConnectionSettings.connectionPool);
+		Connection oConnection = oConnectionImplementation.newConnection();
+		PostDao oPostDao = new PostDao(oConnection);
+		Gson oGson = GsonFactory.getGson();
+		int id = Integer.parseInt(oRequest.getParameter("id"));
+		if (oPostDao.remove(id) == 0) {
+			oResponseBean = new ResponseBean(500, "KO");
+		} else {
+			oResponseBean = new ResponseBean(200, "OK");
+		}
+		if (oConnection != null) {
+			oConnection.close();
+		}
+		if (oConnectionImplementation != null) {
+			oConnectionImplementation.disposeConnection();
+		}
+		return oGson.toJson(oResponseBean);
+	}
 
-    public String fill() throws SQLException {
-        ConnectionInterface oConnectionImplementation = ConnectionFactory.getConnection(ConnectionSettings.connectionPool);
-        Connection oConnection = oConnectionImplementation.newConnection();
-        PostDao oPostDao = new PostDao(oConnection);
-        Gson oGson = GsonFactory.getGson();
-        Date date1 = new GregorianCalendar(2014, Calendar.JANUARY, 1).getTime();
-        Date date2 = new GregorianCalendar(2019, Calendar.DECEMBER, 31).getTime();
-        int numPost = Integer.parseInt(oRequest.getParameter("number"));
-        for (int i = 0; i < numPost; i++) {
-            PostBean oPostBean = new PostBean();
-            Date randomDate = new Date(ThreadLocalRandom.current()
-                    .nextLong(date1.getTime(), date2.getTime()));
-            oPostBean.setTitulo(generaTexto(1));
-            oPostBean.setCuerpo(generaTexto(5));
-            oPostBean.setEtiquetas(generaTexto(1));
-            oPostBean.setFecha(randomDate);
-            oPostDao.insert(oPostBean);
-        }
-        ResponseBean oResponseBean = new ResponseBean(200, "Insertados los registros con exito");
-        if (oConnection != null) {
-            oConnection.close();
-        }
-        if (oConnectionImplementation != null) {
-            oConnectionImplementation.disposeConnection();
-        }
-        return oGson.toJson(oResponseBean);
-    }
+	public String fill() throws SQLException {
+		ConnectionInterface oConnectionImplementation = ConnectionFactory
+				.getConnection(ConnectionSettings.connectionPool);
+		Connection oConnection = oConnectionImplementation.newConnection();
+		PostDao oPostDao = new PostDao(oConnection);
+		Gson oGson = GsonFactory.getGson();
+		Date date1 = new GregorianCalendar(2014, Calendar.JANUARY, 1).getTime();
+		Date date2 = new GregorianCalendar(2019, Calendar.DECEMBER, 31).getTime();
+		int numPost = Integer.parseInt(oRequest.getParameter("number"));
+		for (int i = 0; i < numPost; i++) {
+			PostBean oPostBean = new PostBean();
+			Date randomDate = new Date(ThreadLocalRandom.current().nextLong(date1.getTime(), date2.getTime()));
+			oPostBean.setTitulo(generaTexto(1));
+			oPostBean.setCuerpo(generaTexto(5));
+			oPostBean.setEtiquetas(generaTexto(1));
+			oPostBean.setFecha(randomDate);
+			oPostDao.insert(oPostBean);
+		}
+		ResponseBean oResponseBean = new ResponseBean(200, "Insertados los registros con exito");
+		if (oConnection != null) {
+			oConnection.close();
+		}
+		if (oConnectionImplementation != null) {
+			oConnectionImplementation.disposeConnection();
+		}
+		return oGson.toJson(oResponseBean);
+	}
 
-    private String generaTexto(int longitud) {
-        String fraseRandom = "";
-        for (int i = 0; i < longitud; i++) {
-            fraseRandom += frasesInicio[(int) (Math.random() * frasesInicio.length) + 0];
-            fraseRandom += frasesMitad[(int) (Math.random() * frasesMitad.length) + 0];
-            fraseRandom += frasesFinal[(int) (Math.random() * frasesFinal.length) + 0];
-        }
-        return fraseRandom;
-    }
+	private String generaTexto(int longitud) {
+		String fraseRandom = "";
+		for (int i = 0; i < longitud; i++) {
+			fraseRandom += frasesInicio[(int) (Math.random() * frasesInicio.length) + 0];
+			fraseRandom += frasesMitad[(int) (Math.random() * frasesMitad.length) + 0];
+			fraseRandom += frasesFinal[(int) (Math.random() * frasesFinal.length) + 0];
+		}
+		return fraseRandom;
+	}
 }
